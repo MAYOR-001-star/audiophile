5b5dbda96f8e85badd04534d5c892dedc50df6c5
MAYOR-001-star <mayorkunabdulazeez01@gmail.com>
2026-09-02 07:41:46 +0100

    feat: implement SubFooter component with dedicated styles and fix lint issues

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
+    // <div className='px-[10.31rem]'>page</div>
     <div>page</div>
   )
 }
--- components/SubFooter.tsx
+++ components/SubFooter.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const SubFooter = () => {
   return (
-    <div>SubFooter</div>
+    <section className='subfooter-container'>
+      <div className='subfooter-image-wrapper'>
+        <Image src='/model.svg' alt='model' fill className='object-cover rounded-[0.50em]' />
+      </div>
+      <div className='subfooter-content'>
+        <h2 className='subfooter-title'>
+          Bringing you the <span className='text-[var(--primary)]'>best</span> audio gear
+        </h2>
+        <p className='footer-text'>
+          Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.
+        </p>
+      </div>
+    </section>
   )
 }
 
